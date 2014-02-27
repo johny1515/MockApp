019d83be91e8ffc6b4689a2f6d83ba87aa48003c
Janusz Wąsik <januszwasik1515@gmail.com>
2014-02-27 14:31:53 +0100

    999

--- ConsoleApplication1/Properties/AssemblyInfo.cs
+++ ConsoleApplication1/Properties/AssemblyInfo.cs
@@ -32,5 +32,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // komentarz
-[assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyFileVersion("1.0.*")]
+// koment 2
+[assembly: AssemblyVersion("2.0.*")]
+[assembly: AssemblyFileVersion("2.0.*")]
